1e0f158a2b781d050721255aa53a62b6b69d2bdc
Rodrigo Rodriguez <rodrigo.rodriguez@cubobit.com>
2021-07-21 14:45:24 -0500

    changes build

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/ucq2/'
+    ? '/egresados2/'
     : '/',
   pages: {
     index: {
